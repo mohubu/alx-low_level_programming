d9547973b562a3fd65e0c93ab9a35239f4962aed
Roy Mohubu <roymohubu@datalabs.mobi>
2023-05-23 14:37:25 -0700

    30islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ * _islower - checks for lowercase character
+ * @c: character to be inspected
+ *
+ * Return: 1 if successful or 0 if uppercase
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
