ccaaaf9d50d577046639ce4664c81bf68fae41c9
Roy Mohubu <roymohubu@datalabs.mobi>
2023-05-24 15:55:09 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add -Our function that adds two integers in our program
+ * @i: first integer
+ * @n: second integer
+ *
+ * Returns: the sum of the two numbers
+ */
+int add(int i, int n)
+{
+	int sum1;
+
+	sum1 = i + n;
+	return (sum1);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,6 +11,7 @@ int _abs(int c);
 int print_last_digit(int c);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
 
 
 
