d57c38d3a01d1a49b2405a6be86715fc20cbc72d
Roy Mohubu <roymohubu@datalabs.mobi>
2023-05-25 15:51:48 -0700

    7-print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - our function that draws a diagonal line in our program
+ * @n: The length of the line
+ *
+ * Return: A diagonal line of length n
+ */
+void print_diagonal(int n)
+{
+	int i, b;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (b = 0; b < i; b++)
+		{
+			_putchar(32);
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -11,3 +11,4 @@
 | `4-print_most_numbers.c` | A function that prints the numbers, from `0` to `9`, followed by a new line. Does not print `2` and `4`. |
 | `5-more_numbers.c` | A function that prints 10 times the numbers, from `0` to `14`, followed by a new line. |
 | `6-print_line.c` | A function that draws a straight line in the terminal. |
+| `7-print_diagonal.c` | A function that draws a diagonal line on the terminal. |
