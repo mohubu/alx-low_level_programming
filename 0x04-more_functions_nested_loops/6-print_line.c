90e4a5e9b25cafdd9cfb24ae4315698eb06a30c4
Roy Mohubu <roymohubu@datalabs.mobi>
2023-05-25 15:06:11 -0700

    Correction

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,13 +9,14 @@ void print_line(int n)
 {
 	int i;
 
+	if (n < 0)
+	{
+		_putchar('\n');
+		return;
+	}
 	for (i = 0; i <= n; i++)
 	{
 		_putchar('_');
-		if (n < 0)
-		{
-			_putchar('\n');
-		}
 	}
 	_putchar('\n');
 }
