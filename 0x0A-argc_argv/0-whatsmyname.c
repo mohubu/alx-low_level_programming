d51611d29dc35e33a8f33c6ca2f18213c835d312
Roy Mohubu <roymohubu@datalabs.mobi>
2023-06-14 13:22:57 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,7 @@
+# 0x0A-argc_argv
+
+## Files 
+
+| Filename | Description |
+| -----------------------|-------------------------------------|
+| `0-whatsmyname.c` | A function that prints its name, followed by a newline. |
