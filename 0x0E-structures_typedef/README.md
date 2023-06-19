741488ffdd6ed5c2a91f435f4a26600d87c4827a
Roy Mohubu <roymohubu@datalabs.mobi>
2023-06-19 14:29:33 -0700

    dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,7 @@
+# 0x0E. C - Structures, typedef
+
+## Files 
+
+| Filename | Description |
+|--------------------------| ----------------------------------|
+| `dog.h` | A new type `struct dog` with the following elements. |
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Our new type struct dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The ower of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
